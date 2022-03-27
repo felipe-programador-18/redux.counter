f35945a41852997c5f0168556c04e8207771bb50
Felipe <machadofelipe2016@outlook.com>
2022-03-27 14:57:37 -0300

    c2- search error

--- redux-counter/src/reducer.js
+++ redux-counter/src/reducer.js
@@ -1,15 +1,13 @@
-
-
 const initialState ={
     count:0
 }
 
-const counterReducer = (state=initialState, action) =>{
+const counterReducer = (state= initialState, action) =>{
     switch(action.type){
         case 'INCREMENT':
-        return { ...state, count: state.count+1}
+          return { ...state,count: state.count+1}
         case 'DECREMENT':
-        return { ...state, count: state.count-1}      
+          return { ...state, count: state.count-1}      
         default:
             return state
     }
