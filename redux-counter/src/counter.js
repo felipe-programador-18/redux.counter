ec3b96aa8b677ff2b01836f7bf596bac86b61668
Felipe <machadofelipe2016@outlook.com>
2022-03-27 14:57:18 -0300

    c1- adding somethings test about error

--- redux-counter/src/counter.js
+++ redux-counter/src/counter.js
@@ -1,16 +1,16 @@
 
 import React, {Component} from "react"
 import { connect } from "react-redux"
-import { decrement, increment } from "./actions"
+import { increment, decrement} from "./actions"
 
 class Counter extends Component{
   render(){
-  return( <>
-        <p>Contador {this.state.count}
+  return( 
+        <p>Contador: {this.state.count}
          <button onClick={this.props.increment} >+</button> 
          <button onClick={this.props.decrement} >-</button> 
         </p>    
-    </>
+   
     )}
 }
 
