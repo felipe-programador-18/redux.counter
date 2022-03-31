fbf9cdfeee3ad3c8d8feaa4d28365fa64ae9a473
Felipe <machadofelipe2016@outlook.com>
2022-03-31 15:36:08 -0300

    c6- adding new date about file counter

--- redux-counter/src/counter.js
+++ redux-counter/src/counter.js
@@ -3,16 +3,26 @@ import React, {Component} from "react"
 import { connect } from "react-redux"
 import { increment, decrement} from "./actions"
 
-class Counter extends Component{
-  render(){
-  return( 
-        <p>Contador: {this.props.count}
-         <button onClick={this.props.increment} >+</button> 
-         <button onClick={this.props.decrement} >-</button> 
-        </p>    
-   
-    )}
+const Counter = ({count, increment, decrement}) =>{
+   return(
+     <p>
+       Contador: {count}
+       <button onClick={increment} >+</button>
+       <button onClick={decrement}>-</button>
+     </p>
+   )
 }
+
+//class Counter extends Component{
+ // render(){
+ // return( 
+   //     <p>Contador: {this.props.count}
+     //    <button onClick={this.props.increment} >+</button> 
+       //  <button onClick={this.props.decrement} >-</button> 
+       // </p>    
+   
+ //   )}
+//}
 
 // now i am mapnig state !!
 const mapStatetoProps = (state) =>{
