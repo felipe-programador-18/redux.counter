a42ce3f854fa5f57b6819ed56abffae4b8a8abff
Felipe <machadofelipe2016@outlook.com>
2022-04-04 15:59:31 -0300

    c16- create all test about action

--- redux-counter/src/actions.test.js
+++ redux-counter/src/actions.test.js
@@ -2,10 +2,17 @@ import { increment, decrement } from "./actions"
 
 
 test("action: increment", () => {
-    let incr = increment(10)
-    expect()
+    let incr = increment(3)
+    expect(incr).toEqual({
+        type: 'INCREMENT',
+        value: 3
+    })
 })
 
 test("action: decrement", () => {
-
+   let decr = decrement(2)
+   expect(decr).toEqual({
+       type: 'DECREMENT',
+       value:2
+   })
 })
