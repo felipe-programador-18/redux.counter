402f04fc14be79a71e38674eccc82f52d1700062
Felipe <machadofelipe2016@outlook.com>
2022-04-04 16:18:36 -0300

    c20- adding test about decrement about reducer

--- redux-counter/src/reducer.test.js
+++ redux-counter/src/reducer.test.js
@@ -1,7 +1,15 @@
 import counterReducer from "./reducer"
 
+
+// this case am testing about reducer
 test('counterReducer', ()=>{
     let state ;
     state = counterReducer({count:0}, {type:'INCREMENT', value:3})
     expect(state).toEqual({count:3})
+})
+
+test('decrement', () => {
+    let state ;
+    state = counterReducer({count:1}, {type:'DECREMENT', value:2})
+    expect(state).toEqual({count:-1})
 })
