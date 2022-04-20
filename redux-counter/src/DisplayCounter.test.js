237888c4bc65f1485463a4f6757c7cbb9dc2be27
Felipe <machadofelipe2016@outlook.com>
2022-04-20 16:11:01 -0300

    c33 try it resolve issues

--- redux-counter/src/DisplayCounter.test.js
+++ redux-counter/src/DisplayCounter.test.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { shallow} from 'enzyme'
-import Enzyme from 'enzyme'
-import {configured} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import {DisplayCounter} from './DisplayCounter'
 
 
-Enzyme.configure({adapter: new Adapter()})
 describe('<DisplayCounter />', () => {
     it('monts', () => {
         const wrapper = shallow(< DisplayCounter count={0} />)
